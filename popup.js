f7af7b511c7dcf499e5dc42710aaeda966f858ac
nathan <nathannash11@gmail.com>
2017-09-04 11:30:31 +0100

    tidied up popup code

--- popup.js
+++ popup.js
@@ -1,33 +1,18 @@
-$(function()
-  {
-	//need to disable jump up animation
+
+//need to disable jump up animation
 
 
-	$(".work img").on("click", function(image){
+function hidePopup()
+{
+	$(".popup-background").css("display","none");
+}
 
-		$(".popup-background").css("display","block");
+function showPopup(title,description,link,github)
+{
+	$(".popup-background").css("display","block");
 
-		var title = image.target.getAttribute("data-name");
-		var text = image.target.getAttribute("data-text");
-		var link = image.target.getAttribute("data-link");
-		var linkGit = image.target.getAttribute("data-link-github");
-
-		setPopup(title,text,link,linkGit);
-
-	});
-
-	$(".popup-exit").on("click",function(){
-			$(".popup-background").css("display","none");
-	});
-
-
-
-	function setPopup(title,description,link,github)
-	{
-		$(".popup-title").text(title);
-		$(".popup-description").text(description);
-		$(".popup-link").attr("href",link);
-		$(".popup-link-github").attr("href",github);
-	}
-
-});
+	$(".popup-title").text(title);
+	$(".popup-description").text(description);
+	$(".popup-link").attr("href",link);
+	$(".popup-link-github").attr("href",github);
+}
